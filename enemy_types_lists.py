6426979b6f5584e2163af420593816abf0191737
eazeon <72609752+FoZIkks@users.noreply.github.com>
2025-08-05 21:09:42 +0200

    Update enemy_types_lists.py

--- enemy_types_lists.py
+++ enemy_types_lists.py
@@ -1,10 +1,49 @@
 enemy_types = [
-    {"name": "Gobelin", "hp": 80, "damage_range": (5, 15), "resistances": {}, "weaknesses": {"piercing": 1.5}, "weight": 3},
-    {"name": "Salamandre", "hp": 100, "damage_range": (5, 15), "resistances": {"fire": 0.5},"weaknesses": {"water": 1.5}, "weight": 1},
-    {"name": "Ogre", "hp": 150, "damage_range": (10, 25), "resistances": {"contondant": 2.0}, "weaknesses": {"magic": 1.25},"weight": 2},
-    {"name": "Spectre", "hp": 70, "damage_range": (5, 10), "resistances": {"contondant": 0.7}, "weaknesses": {"magic": 2.0}, "weight": 1},
+    {
+        "name": "Gobelin",
+        "hp": 80,
+        "damage_range": (5, 15),
+        "resistances": {},
+        "weaknesses": {"piercing": 1.5},
+        "weight": 3,
+        "bounty": 50
+    },
+    {
+        "name": "Salamandre",
+        "hp": 100,
+        "damage_range": (5, 15),
+        "resistances": {"fire": 0.5},
+        "weaknesses": {"water": 1.5},
+        "weight": 1,
+        "bounty": 75
+    },
+    {
+        "name": "Ogre",
+        "hp": 150,
+        "damage_range": (10, 25),
+        "resistances": {"contondant": 0.7},
+        "weaknesses": {"magic": 1.25},
+        "weight": 2,
+        "bounty": 150
+    },
+    {
+        "name": "Spectre",
+        "hp": 70,
+        "damage_range": (5, 10),
+        "resistances": {"contondant": 0.7},
+        "weaknesses": {"magic": 2.0},
+        "weight": 1,
+        "bounty": 125
+    },
 ]
 
 boss_list = [
-    {"name": "Golem de pierre", "hp": 250, "damage_range": (25, 40), "resistances": {"piercing": 0.7}, "weaknesses": {"magic": 2.0}, "weight": 1},
+    {
+        "name": "Golem de pierre",
+        "hp": 250,
+        "damage_range": (25, 40),
+        "resistances": {"piercing": 0.7},
+        "weaknesses": {"magic": 2.0},
+        "weight": 1
+    },
 ]
